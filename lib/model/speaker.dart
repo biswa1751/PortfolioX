c44a91fef5a89f156927d0dd98fd8a7fa687231b
Biswajit Behera <suraj53028@gmail.com>
2020-06-18 20:11:49 +0530

    add speaker model file

---
+++ lib/model/speaker.dart
@@ -0,0 +1,20 @@
+class Speaker {
+  final String name;
+  final String imageUrl;
+  final String twitterUrl;
+  final String instagramUrl;
+  final String youtubeUrl;
+  final String githubUrl;
+  final String introductionText;
+  final String profileUrl;
+  Speaker( {
+    this.name,
+    this.imageUrl,
+    this.twitterUrl,
+    this.instagramUrl,
+    this.youtubeUrl,
+    this.githubUrl,
+    this.introductionText,
+    this.profileUrl,
+  });
+}
